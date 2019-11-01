099e67f542498ea49b6db7e619a19786bd1f7263
samuelvary1 <svary@triplelift.com>
2019-11-01 15:13:22 -0400

    finish initial Movie.ts entity

--- src/lib/entities/Movie.ts
+++ src/lib/entities/Movie.ts
@@ -16,6 +16,68 @@ export interface Movie {
  Awards: string;
  Poster: string;
  Ratings: AnyObject[];
-
-
+ Metascore: string;
+ imdbRating: string;
+ imdbVotes: string;
+ imdbID: string;
+ Type: string;
+ DVD: string;
+ BoxOffice: string;
+ Production: string;
+ Website: string;
+ Response: boolean;
 }
+
+export const makeMovie = (
+	Title: string = '',
+	Year: string = '',
+	Rated: string = '',
+	Released: string = '',
+	Runtime: string = '',
+	Genre: string = '',
+	Director: string = '',
+	Writer: string = '',
+	Actors: string = '',
+	Plot: string = '',
+	Language: string = '',
+	Country: string = '',
+	Awards: string = '',
+	Poster: string = '',
+	Ratings: AnyObject[],
+	Metascore: string = '',
+	imdbRating: string = '',
+	imdbVotes: string = '',
+	imdbID: string = '',
+	Type: string = '',
+	DVD: string = '',
+	BoxOffice: string = '',
+	Production: string = '',
+	Website: string = '',
+	Response: boolean = true
+): Movie => ({
+	Title,
+	Year,
+	Rated,
+	Released,
+	Runtime,
+	Genre,
+	Director,
+	Writer,
+	Actors,
+	Plot,
+	Language,
+	Country,
+	Awards,
+	Poster,
+	Ratings,
+	Metascore,
+	imdbRating,
+	imdbVotes,
+	imdbID,
+	Type,
+	DVD,
+	BoxOffice,
+	Production,
+	Website,
+	Response
+})
