6b73b16266400d166a910ef650c4963c6a4c2322
samuelvary1 <svary@triplelift.com>
2019-11-04 17:26:23 -0500

    set up basic attempt at fetch in MovieService

---
+++ src/lib/providers/Movie/MovieProvider.tsx
@@ -0,0 +1,1 @@
+import movieService from "../../services/Movie/MovieServices";
--- src/lib/services/Movie/MovieService.ts
+++ src/lib/services/Movie/MovieService.ts
@@ -1,1 +1,32 @@
 import Base from "../Base";
+import fetch from "../Fetch"
+import { makeMovie, Movie } from "../../entities/Movie";
+import { jsonHydrateEntity } from "../../../utils/jsonHydration";
+
+class MovieService extends Base {
+  public constructor() {
+    super();
+  }
+
+  async fetchMovie(Title: string = "", init: RequestInit = {}): Promise<Movie> {
+    try {
+      const resp = await fetch(`${this.apiUrl}/?apikey=${this.apiKey}${Title ? `&t=${Title}` : ''}`, {
+        ...init,
+        method: 'GET'
+      });
+
+      const { movie } = await resp.json();
+
+      const value = jsonHydrateEntity(movie, makeMovie());
+
+      return value;
+    } catch (e) {
+      if (e.error_details) {
+        throw e.error_details;
+      }
+      throw new Error(e.message || 'Unknown error.');
+    }
+  }
+}
+
+export default new MovieService();
